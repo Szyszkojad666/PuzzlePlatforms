791a85607d4769e2e4659246f305afb536a0f881
Szyszkojad666 <rozmarynowski.r@gmail.com>
2019-03-24 21:13:12 +0100

    Lobby map Host function with server travel Join function with client travel

--- PuzzlePlatforms/Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
+++ PuzzlePlatforms/Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -20,11 +20,21 @@ void UPuzzlePlatformsGameInstance::Host()
 	if (Engine)
 	{
 		Engine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, TEXT("Host"));
+		UWorld* World = GetWorld();
+		if (World)
+		{
+			World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen");
+		}
 	}
 }
 
 void UPuzzlePlatformsGameInstance::Join(const FString& Address)
 {
+	APlayerController* PC = GetFirstLocalPlayerController();
+	if (PC)
+	{
+		PC->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
+	}
 	GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, FString::Printf(TEXT("Joining %s"), *Address)); // printf is needed to convert TEXT into FString and print it together with Address
 }
 
