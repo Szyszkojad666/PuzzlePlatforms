2d3dfd976da2e29e46a67965b27e0a1db56389fd
Szyszkojad666 <rozmarynowski.r@gmail.com>
2019-05-21 20:21:27 +0200

    Seamless travel when lobby is populated with at least 2 players Transition map

--- PuzzlePlatforms/Source/PuzzlePlatforms/LobbyGameMode.cpp
+++ PuzzlePlatforms/Source/PuzzlePlatforms/LobbyGameMode.cpp
@@ -1,17 +1,23 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "LobbyGameMode.h"
+#include "Runtime/Engine/Classes/Engine/World.h"
 
 void ALobbyGameMode::PostLogin(APlayerController * NewPlayer)
 {
+	Super::PostLogin(NewPlayer);
 	++PlayerCount;
-	if (PlayerCount >= 3)
+	if (PlayerCount >= 2)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("3 players in the lobby"));
+		UWorld* World = GetWorld();
+		if (!ensure(World != NULL)) return;
+		bUseSeamlessTravel = true;
+		World->ServerTravel("/Game/Maps/L_Game?listen");
 	}
 }
 
 void ALobbyGameMode::Logout(AController * Exiting)
 {
-	PlayerCount--;
+	Super::Logout(Exiting);
+	--PlayerCount;
 }
